1388f0c9b9d9b830c9162a7d0815bac6eb94b39f
jimcavoy <jimcavoy@thetastream.com>
2020-09-14 20:14:09 +0000

    Added a list view to Session Detail page that displays shooting bouts.

--- BiStatApp/BiStatApp/Models/ShootingBout.cs
+++ BiStatApp/BiStatApp/Models/ShootingBout.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using Xamarin.Forms;
 
 namespace BiStatApp.Models
 {
@@ -30,5 +32,35 @@ namespace BiStatApp.Models
 
 		public int SessionId { get; set; }
 		public Session Session { get; set; }
+
+		[NotMapped]
+		public int Misses
+		{
+			get 
+			{
+				int m = 0;
+
+				m = Alpha ? 0 : 1;
+				m += Bravo ? 0 : 1;
+				m += Charlie ? 0 : 1;
+				m += Delta ? 0 : 1;
+				m += Echo ? 0 : 1;
+
+				return m;
+			}
+		}
+
+		//[NotMapped]
+		//public ImageSource PositionImage
+		//{
+		//	get
+		//	{
+		//		if (Position == PositionEnum.PRONE)
+		//		{
+		//			return ImageSource.FromResource("BiProne.png");
+		//		}
+		//		return ImageSource.FromResource("BiStanding.png");
+		//	}
+		//}
 	}
 }
