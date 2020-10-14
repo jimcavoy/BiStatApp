48857a8dda49127cb2bc2f5eba2d3143dd16ab48
jimcavoy <jimcavoy@thetastream.com>
2020-10-14 00:03:13 +0000

    Import Font Awesome fonts into the solution.  Start using their glyphs for images.

--- BiStatApp/BiStatApp/AssemblyInfo.cs
+++ BiStatApp/BiStatApp/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesome5Brands.otf", Alias = "FontAwesomeBrands")]
+[assembly: ExportFont("FontAwesome5Regular.otf", Alias = "FontAwesomeRegular")]
+[assembly: ExportFont("FontAwesome5Solid.otf", Alias = "FontAwesomeSolid")]
--- BiStatApp/BiStatApp/ViewModels/MainPageMasterViewModel.cs
+++ BiStatApp/BiStatApp/ViewModels/MainPageMasterViewModel.cs
@@ -16,11 +16,17 @@ namespace BiStatApp.ViewModels
 
         public MainPageMasterViewModel()
         {
+            Color color = Color.DarkGray;
+            var ret = Application.Current.Resources.TryGetValue("MediumGrayTextColor", out var outValue);
+            if (ret && outValue is Color)
+            {
+                color = (Color)outValue;
+            }
             MenuItems = new ObservableCollection<MainPageMasterMenuItem>(new[]
             {
-                new MainPageMasterMenuItem { Id = 0, Title = "Train", TargetType = typeof(PracticesPage), Icon = ImageSource.FromResource("BiStatApp.Assets.Images.target.png")},
-                new MainPageMasterMenuItem { Id = 1, Title = "History", TargetType = typeof(SessionsPage), Icon = ImageSource.FromResource("BiStatApp.Assets.Images.todo.png")},
-                new MainPageMasterMenuItem { Id = 2, Title = "Settings", TargetType = typeof(SettingsPage), Icon = ImageSource.FromResource("BiStatApp.Assets.Images.settings.png")}
+                new MainPageMasterMenuItem { Id = 0, Title = "Train", TargetType = typeof(PracticesPage), Icon = new FontImageSource() { FontFamily = "FontAwesomeSolid", Glyph="\uf140", Color=color} },
+                new MainPageMasterMenuItem { Id = 1, Title = "History", TargetType = typeof(SessionsPage), Icon = new FontImageSource() { FontFamily = "FontAwesomeSolid", Glyph="\uf03a", Color=color} },
+                new MainPageMasterMenuItem { Id = 2, Title = "Settings", TargetType = typeof(SettingsPage), Icon = new FontImageSource() { FontFamily = "FontAwesomeSolid", Glyph="\uf013", Color=color} }
             });
         }
     }
